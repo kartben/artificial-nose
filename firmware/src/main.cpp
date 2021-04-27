dd46bee99dc9d6a2583fccbef96b3c26249cfa3f
Benjamin Cabé <kartben@users.noreply.github.com>
2021-04-27 16:52:10 +0200

    remove unused function

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,19 +1,6 @@
-#include <unistd.h>
-int freeMemory() {
-  char top;
-#ifdef __arm__
-  return &top - reinterpret_cast<char*>(sbrk(0));
-#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
-  return &top - __brkval;
-#else  // __arm__
-  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
-#endif  // __arm__
-}
-
 #include <Arduino.h>
 #include "Config.h"
 #include "ConfigurationMode.h"
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Storage
